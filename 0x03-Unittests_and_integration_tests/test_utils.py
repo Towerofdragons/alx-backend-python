7d649b839f959223421607a9b97ad664bfd884e2
Mark <marknjoropge@gmail.com>
2024-11-04 00:24:08 +0000

    Add task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,6 +21,20 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected_result: Any) -> None:
         self.assertEqual(utils.access_nested_map(map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ["a"], KeyError, "'a'"),
+        ({"a": 1}, ["a", "b"], KeyError, "'b'")
+    ])
+    def test_access_nested_map_exception(self, map: Dict[Any, Any],
+                                         path: List[str],
+                                         expected_result: Any,
+                                         msg: str) -> None:
+        with self.assertRaises(expected_result) as cm:
+            utils.access_nested_map(map, path)
+
+        error = str(cm.exception)
+        self.assertEqual(error, msg)
+
 
 if __name__ == "__main__":
     unittest.main()
